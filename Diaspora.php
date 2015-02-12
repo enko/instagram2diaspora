03e5d79a33778a7a0d7367ceed115f5df821ed6c
Tim Schumacher <tim@bandenkrieg.hacked.jp>
2015-02-12 19:15:26 +0100

    Add a /licence/

--- Diaspora.php
+++ Diaspora.php
@@ -1,11 +1,13 @@
 <?php
-
-/**
- * Created by PhpStorm.
- * User: tim
- * Date: 12.02.2015
- * Time: 17:03
+/*
+ * ----------------------------------------------------------------------------
+ * "THE VODKA-WARE LICENSE" (Revision 42):
+ * <tim@datenkonten.me> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a vodka in return.     Tim Schumacher
+ * ----------------------------------------------------------------------------
  */
+
 class Diaspora
 {
     private $cookie = '';
